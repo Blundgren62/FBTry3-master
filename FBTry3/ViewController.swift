a1b7b586a1c824d238d6fa5e28cead84c941ccfb
Blundgren <blundgren@icloud.com>
2019-04-10 05:52:09 +0200

    Initial Commit

---
+++ FBTry3/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  FBTry3
+//
+//  Created by Bent Lundgren on 03/04/2019.
+//  Copyright © 2019 Bent Lundgren. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var FirebaseLabel: UILabel!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let db = Firestore.firestore()
+ 
+        // Add a new document with a generated ID
+        var ref: DocumentReference? = nil
+        ref = db.collection("users").addDocument(data: [
+            "first": "Alan",
+            "middle": "Mathison",
+            "last": "Turing",
+            "born": 1912
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+        
+        db.collection("users").getDocuments { (QuerySnapshot, err) in
+            if let err = err {
+                print("error getting documents: \(err)")
+            }else {
+                for document in QuerySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                    self.FirebaseLabel.text = "\(document.data())"
+                }
+            }
+        }
+
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '12.2'
+
+target 'FBTry3' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FBTry3
+pod 'Firebase/Core'
+pod 'Firebase/Firestore'
+
+
+
+
+end
