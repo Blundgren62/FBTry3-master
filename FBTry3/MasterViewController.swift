363c7562ec8cc29a47baddfd78f18f990d89bbb6
Blundgren <blundgren@icloud.com>
2019-05-03 05:54:35 +0200

    Der er sat lidt ind forskelligt

---
+++ FBTry3/MasterViewController.swift
@@ -0,0 +1,145 @@
+//
+//  MasterViewController.swift
+//  FBTry3
+//
+//  Created by Bent Lundgren on 30/04/2019.
+//  Copyright © 2019 Bent Lundgren. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class MasterViewController: UITableViewController {
+    
+    @IBOutlet weak var addButton: UIBarButtonItem!
+
+    
+    private var documents: [DocumentSnapshot] = []
+    public var tasks: [Task] = []
+    private var listener : ListenerRegistration!
+    
+    
+    fileprivate func baseQuery() -> Query {
+        return Firestore.firestore().collection("Task").limit(to: 50)
+        
+    }
+    
+    fileprivate var query: Query? {
+        didSet {
+            if let listener = listener {
+                listener.remove()
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.query = baseQuery()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.listener.remove()
+    }
+
+    override func viewWillAppear(_ _animated: Bool) {
+        self.listener =  query?.addSnapshotListener { (documents, error) in
+            guard let snapshot = documents else {
+                print("Error fetching documents results: \(error!)")
+                return
+            }
+            
+            let results = snapshot.documents.map { (document) -> Task in
+                if let task = Task(dictionary: document.data(), id: document.documentID) {
+                    return task
+                } else {
+                    fatalError("Unable to initialize type \(Task.self) with dictionary \(document.data())")
+                }
+            }
+            
+            self.tasks = results
+            self.documents = snapshot.documents
+            self.tableView.reloadData()
+            
+        }
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let item = tasks[indexPath.row]
+  //      print(item)
+        cell.textLabel!.text = item.name
+        cell.textLabel!.textColor = item.done == false ? UIColor.black : UIColor.lightGray
+        
+        return cell
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FBTry3/Task.swift
@@ -0,0 +1,32 @@
+//
+//  Task.swift
+//  FBTry3
+//
+//  Created by Bent Lundgren on 30/04/2019.
+//  Copyright © 2019 Bent Lundgren. All rights reserved.
+//
+
+import Foundation
+
+struct Task{
+    var name:String
+    var done: Bool
+    var id: String
+    
+    var dictionary: [String: Any] {
+        return [
+            "name": name,
+            "done": done
+        ]
+    }
+}
+
+extension Task{
+    init?(dictionary: [String : Any], id: String) {
+        guard   let name = dictionary["name"] as? String,
+            let done = dictionary["done"] as? Bool
+            else { return nil }
+        
+        self.init(name: name, done: done, id: id)
+    }
+}
